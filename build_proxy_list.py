554ca8000fa3b2a9deca4f658d1335a432193372
headrx <joshbelmar@gmail.com>
2019-03-22 11:57:15 -0500

    Added build_proxy_list.py

---
+++ build_proxy_list.py
@@ -0,0 +1,45 @@
+import requests
+from bs4 import BeautifulSoup
+
+def retrieve_latest_link(session):
+        
+        response = session.get('https://pastebin.com/u/DavidStorm', headers=headers)
+        #Get the source and open in soup
+        page_source = response.content
+        soup = BeautifulSoup(page_source, 'html.parser')
+        #Find our table and grab the link for latest post
+        latest_pastes = soup.find('table', class_="maintable")
+        link = latest_pastes.find('a')
+        return "https://pastebin.com/raw"+link['href']
+
+def get_list(url, session):
+    proxies = []
+    page_source = session.get(url, headers=headers).text
+    #Create file / empty file if already present
+    with open('proxies.txt', 'w') as proxy_file:
+        proxy_file.writelines(page_source)
+    #Open file, read in contents, grab ip address's, append to list, write to file
+    with open('proxies.txt', 'r+') as proxy_file:
+        data = proxy_file.readlines()
+        for line in data:
+            try:
+                current_line = line.split()
+                ip = current_line[2]
+                proxies.append(ip)
+                print('[+] Proxy Added : ', ip)
+            except:
+                pass
+    with open('proxies.txt', 'w') as proxy_file:
+        for ip in proxies:
+            proxy_file.write(ip+"\n")
+    print('[+] Completed')
+
+#Build session and add UA to headers
+session = requests.Session()
+headers = {"user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36"}
+#Get link for daily post
+url = retrieve_latest_link(session)
+#Get contents of pastebin page, clean, and output to proxies.txt
+get_list(url, session)
+
+
