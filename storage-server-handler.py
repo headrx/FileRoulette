9e6cb73fba91a4540b56ef924524311b69ffb414
headrx <joshbelmar@gmail.com>
2019-03-29 09:46:34 -0500

    Fixed JSON creation bug

--- storage-server-handler.py
+++ storage-server-handler.py
@@ -1,7 +1,7 @@
 
 import os
 import json
-from flask import Flask, render_template, Markup
+from flask import Flask, render_template, Markup, redirect
 app = Flask(__name__)
 
 """ Server to save filedata from fileroulette to JSON db utilizing simple http requests. """
@@ -10,10 +10,11 @@ app = Flask(__name__)
 
 @app.route('/submit', methods=['POST','GET'])
 @app.route('/submit/<module>/<url_number>', methods=['POST','GET'])
+@app.route('/submit/<module>/<url_number>/<file_name>', methods=['POST','GET'])
 @app.route('/submit/<module>/<url_number>/<file_name>/<file_size>', methods=['POST','GET'])
 @app.route('/submit/<module>/<url_number>/<file_name>/<file_size>/<expiration>', methods=["POST", "GET"])
 def submit(module=None,url_number=None,file_name=None,file_size=None, expiration=None):
-    basic_usage= """
+    basic_usage= """<pre>
                                         -----Basic user information-----\n\n
                                 -- localhost:5000/submit will show this message--\n\n
 
@@ -28,12 +29,12 @@ def submit(module=None,url_number=None,file_name=None,file_size=None, expiration
                     http://localhost:5000/submit/gofile/123456/test.txt/   Bare minimum plus file name
                     http://localhost:5000/submit/gofile/123456/test.txt/3mb/ Bare minimum plus file name, and file size
                     http://localhost:5000/submit/gofile/123456/test.txt/3mb/4-20-1922 full request - has module,string found with urlgen, filename,filesize, expiration date
-                    """
+                </pre>    """
 
     data = load_json()
     #If its just a base /submit , then it will return the message explaining the format
     if module ==None and url_number == None and file_name==None and file_size==None and expiration==None:
-        return Markup("<pre>{}</pre>".format(basic_usage))
+        return Markup("{}".format(basic_usage))
 
     #If at least the module name and url number are present
     elif module!=None and url_number !=None:
@@ -41,6 +42,7 @@ def submit(module=None,url_number=None,file_name=None,file_size=None, expiration
         print('Url: {} File: {} File Size:{} Expiration: {}'.format(modules[module], file_name, file_size, expiration))
         data['modules'][module][url_number] = {"url":modules[module],"file_name":file_name, "file_size":file_size, "expiration": expiration}
         write_json(data)
+        print("Module: {} Url: {} file_name: {} file_size:{} expiration:{}".format(module,modules[module],file_name,file_size,expiration))
         return "Module: {} Url: {} file_name: {} file_size:{} expiration:{}".format(module,modules[module],file_name,file_size,expiration)
 
 def load_json():
@@ -61,8 +63,8 @@ def load_json():
                     }
                 }
             }
-            json_data = json.dumps(data)
-            print("Data is : {}".format(data))
+            json_data = json.dumps({"modules":{"gofile":{},"upfile":{}}})
+            #json_data['modules'] = {'gofile':{},'upfile':{}}
             write_json(json_data)
     return data
 
